cb67de0d061106fe3ff87c4c1a29286504f26ee3
Sithu-BinaryLab <sithulwin.binarylab@gmail.com>
2024-03-19 11:22:21 +0630

    update: package json update

--- src/assets/icons/radio-icon.jsx
+++ src/assets/icons/radio-icon.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const CrossIcon = () => {
   return (
     <div className="cursor-pointer">
-      
-<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="7.5" cy="7.5" r="7" fill="#6364F6" stroke="#6364F6"/>
-</svg>
-
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="7.5" cy="7.5" r="7" fill="#6364F6" stroke="#6364F6" />
+      </svg>
     </div>
   );
 };
